2d9b33015d5c121b503d2873ddc5636048df54c8
Dennis Roberts <dennis@iplantcollaborative.org>
2012-03-27 10:53:31 -0700

    CORE-2973: create an empty-response servlet that we can use as the service URL for CAS session keepalive requests

---
+++ src/main/java/org/iplantc/de/server/EmptyResponseServlet.java
@@ -0,0 +1,23 @@
+package org.iplantc.de.server;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * A servlet that does nothing but return an empty HTTP response.
+ * 
+ * @author Dennis Roberts
+ */
+public class EmptyResponseServlet extends HttpServlet {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setStatus(HttpServletResponse.SC_OK);
+    }
+}
