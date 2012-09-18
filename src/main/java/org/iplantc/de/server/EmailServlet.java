28c3d73ee72b4a6cc787212080cb0defdb87c017
Dennis Roberts <dennis@iplantcollaborative.org>
2012-09-17 18:16:10 -0700

    CORE-3495: removed the unused SessionInitializationServlet and modified EmailServlet so that it forwards message requests to the iPlant email service

--- src/main/java/org/iplantc/de/server/EmailServlet.java
+++ src/main/java/org/iplantc/de/server/EmailServlet.java
@@ -1,41 +1,30 @@
 package org.iplantc.de.server;
 
-import java.io.IOException;
-import java.util.Properties;
-
-import javax.mail.Message;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
-import javax.servlet.ServletConfig;
-import javax.servlet.ServletException;
-
 import org.iplantc.de.shared.services.EmailService;
 
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+import org.iplantc.de.server.service.IplantEmailClient;
+import org.iplantc.de.server.service.IplantEmailClient.MessageRequest;
 
 /**
  * A servlet for sending simple emails. The server address is read from the mail.smtp.host property.
- * 
+ *
  * @author hariolf
- * 
+ *
  */
 public class EmailServlet extends RemoteServiceServlet implements EmailService {
     private static final long serialVersionUID = -3893564670515471591L;
-    private static final String PROPERTIES_FILE = "email.properties"; //$NON-NLS-1$
-    private Properties properties;
 
-    @Override
-    public void init(ServletConfig config) throws ServletException {
-        super.init(config);
-        properties = new Properties();
-        try {
-            properties.load(Thread.currentThread().getContextClassLoader()
-                    .getResourceAsStream(PROPERTIES_FILE));
-        } catch (IOException e) {
-            throw new ServletException("Can't load file " + PROPERTIES_FILE, e); //$NON-NLS-1$
-        }
+    /**
+     * The client used to send message requests to the iPlant email service.
+     */
+    private final IplantEmailClient client;
+
+    /**
+     * @param client the client used to send message requests to the iPlant email service.
+     */
+    public EmailServlet(IplantEmailClient client) {
+        this.client = client;
     }
 
     /**
@@ -43,18 +32,11 @@ public class EmailServlet extends RemoteServiceServlet implements EmailService {
      */
     @Override
     public void sendEmail(String subject, String message, String fromAddress, String toAddress) {
-        Properties props = System.getProperties();
-
-        Session session = Session.getDefaultInstance(props, null);
-        MimeMessage mimeMsg = new MimeMessage(session);
-        try {
-            mimeMsg.setFrom(new InternetAddress(fromAddress));
-            mimeMsg.addRecipient(Message.RecipientType.TO, new InternetAddress(toAddress));
-            mimeMsg.setSubject(subject);
-            mimeMsg.setText(message);
-            Transport.send(mimeMsg);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+        MessageRequest request = new MessageRequest()
+                .setSubject(subject)
+                .setContent(message)
+                .setFromAddress(fromAddress)
+                .setToAddress(toAddress);
+        client.sendMessage(request);
     }
 }
