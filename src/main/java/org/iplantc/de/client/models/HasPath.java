5cd68717bbf9fc159acf9ccb011940206a453954
Jonathan Strootman <jstroot@iplantcollaborative.org>
2014-02-17 17:28:00 -0700

    Cleaned ui-diskresource service and model packages

--- src/main/java/org/iplantc/de/client/models/HasPath.java
+++ src/main/java/org/iplantc/de/client/models/HasPath.java
@@ -1,10 +1,15 @@
 package org.iplantc.de.client.models;
 
-import com.google.web.bindery.autobean.shared.AutoBean.PropertyName;
+import com.google.web.bindery.autobean.shared.AutoBean;
 
+/**
+ * An interface for an {@link AutoBean} which has a "path" key.
+ * 
+ * @author jstroot
+ * 
+ */
 public interface HasPath {
-
-    @PropertyName("path")
-    public String getPath();
     
+    String getPath();
+
 }
