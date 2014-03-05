05bcef25b3402d53d5047cab31d663d86f9cdb96
Jonathan Strootman <jstroot@iplantcollaborative.org>
2014-03-05 14:27:20 -0700

    CORE-5196: Corrected Gin bindings from previous commit

--- src/main/java/org/iplantc/de/client/gin/ServicesModule.java
+++ src/main/java/org/iplantc/de/client/gin/ServicesModule.java
@@ -2,8 +2,12 @@ package org.iplantc.de.client.gin;
 
 import org.iplantc.de.client.models.UserInfo;
 import org.iplantc.de.client.services.DEServiceFacade;
+import org.iplantc.de.client.services.DefaultToolRequestProvider;
 import org.iplantc.de.client.services.DiskResourceServiceFacade;
+import org.iplantc.de.client.services.DiskResourceServiceFacadeImpl;
+import org.iplantc.de.client.services.SearchServiceFacade;
 import org.iplantc.de.client.services.ToolRequestProvider;
+import org.iplantc.de.client.services.impl.SearchServiceFacadeImpl;
 
 import com.google.gwt.inject.client.AbstractGinModule;
 import com.google.inject.Provides;
@@ -13,8 +17,10 @@ final class ServicesModule extends AbstractGinModule {
 
     @Override
     protected void configure() {
-        bind(DiskResourceServiceFacade.class).in(Singleton.class);
-        bind(ToolRequestProvider.class).in(Singleton.class);
+        bind(DiskResourceServiceFacade.class).to(DiskResourceServiceFacadeImpl.class).in(Singleton.class);
+        bind(ToolRequestProvider.class).to(DefaultToolRequestProvider.class).in(Singleton.class);
+
+        bind(SearchServiceFacade.class).to(SearchServiceFacadeImpl.class);
     }
 
     @Provides
